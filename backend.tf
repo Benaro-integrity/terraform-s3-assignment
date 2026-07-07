251539b9c16cfed6666c84e78d1b51ae0f57ff50
Bernard Kermah <benaro.integrity@gmail.com>
2026-07-07 19:20:03 +0000

    added comment and changed region

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
+/*This my tf comment */
 terraform {
   backend "s3" {
     bucket = "my-completely-unique-bucket-name-2026"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-west-2"
   }
 }
