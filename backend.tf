d693d3f72997c087ef1e3426c91316303dfa3a13
Bernard Kermah <benaro.integrity@gmail.com>
2026-07-08 18:15:46 +0000

    updated s3 bucket name in  backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "jenkins-terraform-s3"
     key     = "s3-bucket-pipeline/terraform.tfstate" # The path/folder structure inside your bucket
-    region  = "eu-west-2"                            # Change this if your 'jenkins-terraform-s3' bucket is in a different region
+    region  = "eu-west-2"                            # Change this if your 'jenkins-terraform-s3-2026' bucket is in a different region
     encrypt = true                                   # Encrypts the state file at rest for security
   }
 }
